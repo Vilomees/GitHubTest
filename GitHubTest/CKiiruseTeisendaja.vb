8b5b4f13935805d938fb9eb0cebb398698d09ac1
Rando Kübar <rakuba@taltech.ee>
2025-03-12 15:30:01 +0200

    Loodud kiiruse teisendaja loogika

---
+++ GitHubTest/CKiiruseTeisendaja.vb
@@ -0,0 +1,11 @@
+﻿Public Class CKiiruseTeisendaja
+    Implements IKiiruseTeisendaja
+
+    Private Function KmhToMs(kmh As Double) As Double Implements IKiiruseTeisendaja.KmhToMs
+        Return kmh / 3.6
+    End Function
+
+    Private Function MsToKmh(ms As Double) As Double Implements IKiiruseTeisendaja.MsToKmh
+        Return ms * 3.6
+    End Function
+End Class
---
+++ GitHubTest/IKiiruseTeisendaja.vb
@@ -0,0 +1,4 @@
+﻿Public Interface IKiiruseTeisendaja
+    Function KmhToMs(ByVal kmh As Double) As Double
+    Function MsToKmh(ByVal ms As Double) As Double
+End Interface
